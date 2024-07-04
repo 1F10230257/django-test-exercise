4f5b0faa27b706ef22cb14b703fc2e892c534917
1F10230257 <s1f102302573@iniad.org>
2024-07-04 14:28:46 +0900

    Flake8修正

--- todo/models.py
+++ todo/models.py
@@ -3,9 +3,9 @@ from django.utils import timezone
 
 # Create your models here.
 class Task(models.Model):
-    title = models.CharField(max_length = 100)
-    completed = models.BooleanField(default = False)
-    posted_at = models.DateTimeField(default = timezone.now)
+    title = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
+    posted_at = models.DateTimeField(default=timezone.now)
     due_at = models.DateTimeField(null=True, blank=True)
 
 
@@ -13,4 +13,4 @@ class Task(models.Model):
         if self.due_at is None:
             return False
         return self.due_at < dt
-
+        
